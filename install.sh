811b76b58eb08134e2473088bafa1c47faad83b1
Raul T <raul.tomita@gmail.com>
2018-05-21 20:52:07 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,25 @@
 # commands that install a new instance of FarmMonitorDaemon
+#!/bin/bash
+
 sudo apt-get update
 sudo apt-get -y install git 
+git clone https://github.com/raultomita/FarmMonitorDaemon.git
 
+#install latest python
+sudo apt-get -y install build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev
+wget https://www.python.org/ftp/python/3.6.5/Python-3.6.5.tar.xz
+tar xf Python-3.6.5.tar.xz
+cd Python-3.6.5
+./configure --enable-optimizations
+make
+sudo make altinstall
 
+#clear build files
+sudo rm -r Python-3.6.5
+rm Python-3.6.5.tar.xz
+sudo apt-get --purge remove build-essential tk-dev
+sudo apt-get --purge remove libncurses5-dev libncursesw5-dev libreadline6-dev
+sudo apt-get --purge remove libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev
+sudo apt-get --purge remove libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev
+sudo apt-get autoremove
+sudo apt-get clean
