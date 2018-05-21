5ebfcaebb456d7d820ed77c10c4dadaa907b4bd5
Raul T <raul.tomita@gmail.com>
2018-05-22 01:24:46 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ sudo apt-get -y install git
 git clone https://github.com/raultomita/FarmMonitorDaemon.git
 
 #install latest python
+#from archive tar -zxvf python....
 sudo apt-get -y install build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev
 wget https://www.python.org/ftp/python/3.6.5/Python-3.6.5.tar.xz
 tar xf Python-3.6.5.tar.xz
@@ -19,9 +20,25 @@ sudo make altinstall
 cd ..
 sudo rm -r Python-3.6.5
 rm Python-3.6.5.tar.xz
-sudo apt-get --purge remove build-essential tk-dev
-sudo apt-get --purge remove libncurses5-dev libncursesw5-dev libreadline6-dev
-sudo apt-get --purge remove libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev
-sudo apt-get --purge remove libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev
+sudo apt-get -y --purge remove build-essential tk-dev
+sudo apt-get -y --purge remove libncurses5-dev libncursesw5-dev libreadline6-dev
+sudo apt-get -y --purge remove libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev
+sudo apt-get -y --purge remove libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev
 sudo apt-get autoremove
 sudo apt-get clean
+
+sudo pip3.6 install --upgrade pip
+sudo pip3.6 install redis
+sudo pip3.6 install gpiozero
+sudo pip3.6 install RPi.GPIO
+
+git clone https://github.com/antirez/redis.git
+
+cd redis
+git checkout 4.0
+make
+sudo make install
+cd utils
+sudo ./install_server.sh
+
+
